99024da1f1b108202bc2cd109f279ab2b8eec538
Fran <lavitz.paco@gmail.com>
2021-02-16 19:49:56 +0100

    8- tabla creada y subida

---
+++ database/migrations/2021_02_16_184747_create_alumnos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string("nombre");
+            $table->string("apellidos");
+            $table->string("DNI")->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
